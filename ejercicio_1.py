c492eae0aa6412f503b21de972878b29773b4f74
MaTaUpla <89056752+MaTaUpla@users.noreply.github.com>
2022-05-27 15:47:29 -0400

    Update ejercicio_1.py

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,19 +1,26 @@
 #Matias Tapia
-#ejercicio 2
+#ejercicio 1
+
+def calculo_letras_por_palabra(palabra, letra):
+  #se inicializa una suma en 0 para contar la letra repetida
+  suma = 0
+  
+  #luego se hace la funcion upper para que cada letra este en mayuscula y se puede contar correctamente si la letra se repite
+  palabra = palabra.upper()
+  letra = letra.upper()
+  #el ciclo for recorre la palabra una por una, con el if se compara la letra con la x
+  for x in palabra:
+    #print(x)
+    if( letra == x):
+      suma += 1
+  return suma
 
 #inicio de variables input
 palabra = input("Ingrese palabra: ")
 letra = input("Ingrese letra: ")
 
-#se inicializa una suma en 0 para contar la letra repetida
-suma = 0
-
-#el ciclo for recorre la palabra una por una, con el if se compara la letra con la x
-#luego se hace la funcion upper para que cada letra este en mayuscula y se puede contar correctamente si la letra se repite
-for x in palabra.upper():
-  #print(x)
-  if( letra.upper() == x):
-    suma += 1
+#uso funcion
+suma = calculo_letras_por_palabra(palabra, letra)
 suma = str(suma)
 palabra = str(palabra)
 letra = str(letra)
