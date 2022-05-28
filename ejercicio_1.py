9551219345e36a47efdaf5e18818b2ea34dd635f
MaTaUpla <89056752+MaTaUpla@users.noreply.github.com>
2022-05-27 20:01:33 -0400

    Update ejercicio_1.py

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,27 +1,39 @@
 #Matias Tapia
-#ejercicio 1
+#Ejercicio 1
 
 def calculo_letras_por_palabra(palabra, letra):
-  #se inicializa una suma en 0 para contar la letra repetida
+  #Se inicializa una suma en 0 para contar la letra repetida
   suma = 0
-  
-  #luego se hace la funcion upper para que cada letra este en mayuscula y se puede contar correctamente si la letra se repite
+  #Luego se hace la funcion upper para que cada letra este en mayuscula y se puede contar correctamente si la letra se repite
   palabra = palabra.upper()
   letra = letra.upper()
-  #el ciclo for recorre la palabra una por una, con el if se compara la letra con la x
+  #El ciclo for recorre la palabra una por una, con el if se compara la letra con la x
   for x in palabra:
-    #print(x)
     if( letra == x):
       suma += 1
   return suma
+  
+#Función principal
+def main():
+  #Inicio de variables input
+  print("############################################")
+  print("Contador de letras repetidas en una palabra")
+  print("############################################")
+  palabra = input("Ingrese palabra: ")
+  letra = input("Ingrese letra: ")
 
-#inicio de variables input
-palabra = input("Ingrese palabra: ")
-letra = input("Ingrese letra: ")
+  #Uso funcion
+  suma = calculo_letras_por_palabra(palabra, letra)
+  if(suma == 1):
+    suma = str(suma)
+    print("La letra '" + letra + "' en la palabra '" + palabra + "' se repite " + suma + " vez")
+  elif(suma == 0):
+    suma = str(suma)
+    print("La letra '" + letra + "' en la palabra '" + palabra + "' nose se repite ninguna vez")
+  else:
+    suma = str(suma)
+    print("La letra '" + letra + "' en la palabra '" + palabra + "' se repite " + suma + " veces")
 
-#uso funcion
-suma = calculo_letras_por_palabra(palabra, letra)
-suma = str(suma)
-palabra = str(palabra)
-letra = str(letra)
-print("La letra '" + letra + "' en la palabra '" +palabra + "' se repte " + suma + " vez/veces")
+#Punto de entrada
+if __name__ == "__main__":
+    main()
